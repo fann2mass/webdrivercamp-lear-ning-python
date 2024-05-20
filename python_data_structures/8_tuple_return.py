952d991b9790f870c7f66a374fbdec6017fa961b
fann2mass <ifann2mass@gmail.com>
2024-05-19 17:52:21 -0700

    Update Strint #2

--- python_data_structures/8_tuple_return.py
+++ python_data_structures/8_tuple_return.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-tuple_return = __import__('8_tuple_return').tuple_return
-list_ = [1, 2, 3, 4, 5]
-result = tuple_return(list_)
-print(result)
-list_len, first_element = tuple_return(list_)
-print(f"List len = {list_len}\nFirst element = {first_element}")
+def tuple_return(arg):
+    if len(arg) == 0:
+        return None
+    else:
+        return len(arg), arg[0]
